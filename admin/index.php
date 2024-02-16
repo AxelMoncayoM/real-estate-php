6a7993406dad117f4fc7ecbbaf5bc27f938907ce
Axel Moncayo <axel.moncayo.dev@gmail.com>
2024-02-16 00:44:39 -0600

    Se agrego una tabla en el index del administrador (solo HTML y CSS)

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,31 @@
       <?php endif; ?>
 
       <a href="/real-estate-php/admin/propiedades/crear.php" class="boton boton-verde">Crear Propiedad</a>
+
+      <table class="propiedades">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Imagen</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Casa en la playa</td>
+            <td><img src="/real-estate-php/imagenes/310e3ebb783b41b5835f25445dc0a1bb.jpg" class="imagen-tabla"></td>
+            <td>Precio</td>
+            <td>
+              <a href="#" class="boton-rojo-block">Eliminar</a>
+              <a href="#" class="boton-verde-block">Actualizar</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </main>
 
 
