907f5905c8410b82539d5b8dc9681b5aa3683e4c
Axel Moncayo <axel.moncayo.dev@gmail.com>
2024-02-19 00:09:19 -0600

    Se modifico la página principal para que la sección en la que se muestran las propiedades para que estas se muestren de manera dinamica desde la base de datos y sin repetir código y se le implemento un limite en las propiedades que se pueden mostrar

--- index.php
+++ index.php
@@ -48,143 +48,15 @@
 
     <section class="seccion contenedor">
       <h2>Casa y Depas en Venta</h2>
-      <div class="contenedor-anuncios">
-        <div class="anuncio">
-          <img
-            src="build/img/anuncio1.jpg"
-            alt="imagen anuncio"
-            loading="lazy"
-          />
-          <div class="contenido-anuncio">
-            <h3>Casa de Lujo en el Lago</h3>
-            <p>Casa en el lago, excelente vista y un magnifico precio</p>
-            <p class="precio">$3,000,000</p>
 
-            <ul class="iconos-caracteristicas">
-              <li>
-                <img
-                  class="caracteristica"
-                  src="build/img/icono_wc.svg"
-                  alt="icono wc"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_estacionamiento.svg"
-                  alt="icono estacionamiento"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_dormitorio.svg"
-                  alt="icono dormitorio"
-                  loading="lazy"
-                />
-                <p>4</p>
-              </li>
-            </ul>
-
-            <a class="boton-naranja" href="anuncios.html">Ver Propiedad</a>
-          </div>
-        </div>
-
-        <div class="anuncio">
-          <img
-            src="build/img/anuncio2.jpg"
-            alt="imagen anuncio"
-            loading="lazy"
-          />
-          <div class="contenido-anuncio">
-            <h3>Casa Terminados de Lujo</h3>
-            <p>Casa en el lago, excelente vista y un magnifico precio</p>
-            <p class="precio">$3,000,000</p>
-
-            <ul class="iconos-caracteristicas">
-              <li>
-                <img
-                  src="build/img/icono_wc.svg"
-                  alt="icono wc"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_estacionamiento.svg"
-                  alt="icono estacionamiento"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_dormitorio.svg"
-                  alt="icono dormitorio"
-                  loading="lazy"
-                />
-                <p>4</p>
-              </li>
-            </ul>
-
-            <a class="boton-naranja" href="anuncios.html">Ver Propiedad</a>
-          </div>
-        </div>
-
-        <div class="anuncio">
-          <img
-            src="build/img/anuncio3.jpg"
-            alt="imagen anuncio"
-            loading="lazy"
-          />
-          <div class="contenido-anuncio">
-            <h3>Casa con Piscina</h3>
-            <p>Casa en el lago, excelente vista y un magnifico precio</p>
-            <p class="precio">$3,000,000</p>
-
-            <ul class="iconos-caracteristicas">
-              <li>
-                <img
-                  src="build/img/icono_wc.svg"
-                  alt="icono wc"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_estacionamiento.svg"
-                  alt="icono estacionamiento"
-                  loading="lazy"
-                />
-                <p>3</p>
-              </li>
-
-              <li>
-                <img
-                  src="build/img/icono_dormitorio.svg"
-                  alt="icono dormitorio"
-                  loading="lazy"
-                />
-                <p>4</p>
-              </li>
-            </ul>
-
-            <a class="boton-naranja" href="anuncios.html">Ver Propiedad</a>
-          </div>
-        </div>
-      </div>
+      
+      <?php 
+        $limite = 3;
+        include("includes/templates/anuncios.php"); 
+      ?>
 
       <div class="ver-todas">
-        <a class="boton-verde" href="anuncios.html">Ver Todas</a>
+        <a class="boton-verde" href="anuncios.php">Ver Todas</a>
       </div>
     </section>
 
