e12e5f79fd6a9d0e1d18ec7fd435d805f547f19c
Axel Moncayo <axel.moncayo.dev@gmail.com>
2024-02-23 13:29:37 -0600

    Se agregaron validaciones al formulario de login

--- login.php
+++ login.php
@@ -1,19 +1,54 @@
 <?php 
+
+  //agregar conexion a la base de datos
+  require "includes/config/database.php";
+  $db = conectar_db();
+
+  //autentificar usuario
+
+  $errores = [];
+
+  if($_SERVER['REQUEST_METHOD'] === "POST" ){
+    var_dump($_POST);
+
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if(!$email){
+      $errores[] = "El email es obligatorio o no es valido";
+    }
+
+    if(!$password){
+      $errores[] = "El password es obligatorio";
+    }
+  }
+
+//Agrega el header
    require 'includes/funciones.php';
    incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion contenido-centrado">
       <h1>Iniciar Sesión</h1>
-      <form action="" class="formulario">
+
+      <?php 
+        foreach($errores as $error):?>
+
+          <div class="alerta error">
+            <?= "$error"; ?>
+          </div>
+      <?php endforeach;?>
+
+
+      <form action="" class="formulario" method="POST" nonvalidate>
           <fieldset>
             <legend>Email y Password</legend>
   
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Tu correo" />
+            <label for="email">E-mail</label>
+            <input type="email" id="email" name="email" placeholder="Tu correo" />
   
             <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Tu password" />
+            <input type="password" id="password" name="password" placeholder="Tu password" />
   
           </fieldset>
 
